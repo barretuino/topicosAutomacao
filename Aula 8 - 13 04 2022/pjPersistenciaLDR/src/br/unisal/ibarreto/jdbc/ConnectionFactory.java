9cc06f868e2594a00d656933e1ca085c4a32a388
Barretuino Tecnologia <41486280+barretuino@users.noreply.github.com>
2022-04-13 20:29:17 -0300

    Add files via upload

---
+++ Aula 8 - 13 04 2022/pjPersistenciaLDR/src/br/unisal/ibarreto/jdbc/ConnectionFactory.java
@@ -0,0 +1,30 @@
+package br.unisal.ibarreto.jdbc;
+/**
+.* Projeto de Gerência.
+ * ConnectionFactory - Fábrica de Conexões com o Banco
+ * @autor Paulo C. Barreto
+ * @date 13/04/2022
+ */
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionFactory {
+	public static Connection getConnection() throws SQLException {
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			return DriverManager.getConnection("jdbc:mysql://192.168.33.26/unisal","paulo","12345679");
+		} catch (ClassNotFoundException e) {
+			throw new SQLException(e.getMessage());
+		}
+	}
+	public static void main(String[] args) {
+		try {
+			ConnectionFactory.getConnection();
+			System.out.println("Conectou");
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+}
