731edbf98abe8054eed4befb7d7dd0f1b3fced4c
Barretuino Tecnologia <41486280+barretuino@users.noreply.github.com>
2022-04-27 19:53:04 -0300

    Add files via upload

---
+++ Aula 10/ReleJava/ReleJava.ino
@@ -0,0 +1,52 @@
+/**
+ * Exemplo de Projeto de Atuação do Arduino
+ * Prof. Paulo Barreto
+ * Data 27/04/2022
+ */
+int val;
+
+void setup()
+{
+  Serial.begin(9600);
+  pinMode(2, OUTPUT);   //Rele 1
+  pinMode(3, OUTPUT);   //Rele 2
+  pinMode(4, OUTPUT);   //Rele 3
+  pinMode(5, OUTPUT);   //Rele 4
+}
+
+void loop(){
+  if (Serial.available()) {
+    val = Serial.read();
+    //Relé 1
+    if (val == 11) {
+      digitalWrite(2, HIGH);    //Liga
+    } 
+    if (val == 10) {
+      digitalWrite(2, LOW);     //Desliga
+    }
+    //Relé 2
+    if (val == 21) {
+      digitalWrite(3, HIGH);
+    } 
+    if (val == 20) {
+      digitalWrite(3, LOW);
+    }
+    //Relé 3
+    if (val == 31) {
+      digitalWrite(4, HIGH);
+    } 
+    if (val == 30) {
+      digitalWrite(4, LOW);
+    }
+    //Relé 4
+    if (val == 41) {
+      digitalWrite(5, HIGH);
+    } 
+    if (val == 40) {
+      digitalWrite(5, LOW);
+    }
+  }else{
+    Serial.print(val);
+  }
+}
+
---
+++ Aula 10/pjGerencia/src/br/unisal/jdbc/ConnectionFactory.java
@@ -0,0 +1,32 @@
+package br.unisal.jdbc;
+/**
+ * Projeto de Gerência
+ * ConnectionFactory - Fábrica de Conexões com o Banco
+ * @autor Paulo C. Barreto
+ * @date 20/04/2022
+ */
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionFactory {
+	public static Connection getConnection() throws SQLException {
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			return DriverManager.getConnection("jdbc:mysql://192.168.32.3/unisal",
+					"paulo","12345679");
+		} catch (ClassNotFoundException e) {
+			throw new SQLException(e.getMessage());
+		}
+	}
+	
+	public static void main(String[] args) {
+		try {
+			ConnectionFactory.getConnection();
+			System.out.println("Conexão realizada com sucesso.");
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+}
