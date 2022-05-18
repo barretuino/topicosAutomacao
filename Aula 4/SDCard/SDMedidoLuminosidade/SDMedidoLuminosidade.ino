46a2a3e9db04d107668b6de154ecee6ced6cc9a0
Barretuino Tecnologia <41486280+barretuino@users.noreply.github.com>
2022-05-18 20:27:55 -0300

    Add files via upload

---
+++ Aula 4/SDCard/SDMedidoLuminosidade/SDMedidoLuminosidade.ino
@@ -0,0 +1,64 @@
+/**
+   Monitoramento de Luminosidade - Arduino Web
+   Uso da persistência em MicroSDCard
+   Prof. Ms. Paulo Barreto
+   Aula: 18/10/2014
+   Exemplo de atualização de leitura de sensor LDR e MicroSDCard
+**/
+#include <SPI.h>
+#include <SD.h>
+
+#define ldr_pin A0 //Pino DATA do Sensor ligado na porta Analogica A0
+
+int valorSensor = 0; //variável usada para ler o valor do sensor em tempo real.
+const int chipSelect = 4;
+
+File medida;
+
+void setup()
+{
+  //Abertura da porta de Comunicação
+  Serial.begin(9600);
+  
+  Serial.print("Inicializando o SDCard....");
+  //Pino utilizado pela SDCard
+  pinMode(10, OUTPUT);
+   
+  //Avalia se foi possível dar inicio a SD
+  if (!SD.begin(chipSelect)) {
+    Serial.println("Falha na detecção do SDCard");    
+    //Aborta e retorna a função
+    return;
+  }
+  Serial.println("SDCard Inicializado");  
+}
+
+void loop()
+{
+  //Realiza a leitura do sensor LDR
+  valorSensor = analogRead(ldr_pin);
+  Serial.print("Medida atual é: ");
+  Serial.print(valorSensor);
+  
+  //Abrir arquivo do SDCard
+  medida = SD.open("medida.txt", FILE_WRITE);
+  
+ //Avalia se foi possível dar inicio a SD
+  if (!SD.begin(chipSelect)) {
+    Serial.println("Falha na detecção do SDCard");    
+    //Aborta e retorna a função
+    return;
+  }
+  
+  //Há um arquivo medida disponível?
+  if (medida) {
+    //Realiza a gravação da medida
+    medida.println(valorSensor);
+    //Realiza o fechamento do arquivo
+    medida.close();
+  } else {
+   //Caso aconteça uma falha na existencia ou acesso ao arquivo
+    Serial.println("Erro ao abrir medida.txt");
+  }
+  delay(1000);
+}
---
+++ Aula 4/SDCard/SdCriaDeletaFile/SdCriaDeletaFile.ino
@@ -0,0 +1,75 @@
+/**
+   Uso da persistência em MicroSDCard
+   Prof. Ms. Paulo Barreto
+   Aula: 18/10/2014
+   Exemplo Básico de utilização do SD Card
+   Este exemplo mostra como criar e destruir um arquivo de cartão SD
+   Cartões SD ligado ao barramento SPI da seguinte forma:
+      MOSI - pin 11
+      MISO - pin 12
+      CLK - pin 13
+      CS - pin 4     
+ */
+#include <SPI.h>
+#include <SD.h>
+
+File myFile;
+
+void setup()
+{
+ // Abre comunicação serial e esperar para a porta abrir :
+  Serial.begin(9600);
+   while (!Serial) {
+    ; // Esperar por porta serial para conectar . Necessário para Leonardo só
+  }
+
+
+  Serial.print("Initializing SD card...");
+  // Na Shield Ethernet, CS é o pino 4 É definido como uma saída por padrão.
+  // Note que mesmo que ele não é usado como o pino CS , o pino SS hardware
+  // (10 na maioria das placas Arduino , 53 no mega ) deve ser deixado como uma saída
+  // ou as funções da biblioteca SD não vai funcionar.
+  pinMode(10, OUTPUT);
+
+  if (!SD.begin(4)) {
+    Serial.println("Inicialização Falhou!");
+    return;
+  }
+  Serial.println("Inicialização Realizada.");
+
+  if (SD.exists("exemplo.txt")) {
+    Serial.println("exemplo.txt existe.");
+  }
+  else {
+    Serial.println("exemplo.txt não existe.");
+  }
+
+  // Abre ou Cria o objeto file
+  Serial.println("Criando exemplo.txt...");
+  myFile = SD.open("exemplo.txt", FILE_WRITE);
+  myFile.close();
+
+  // Checa se o objeto file existe
+  if (SD.exists("exemplo.txt")) {
+    Serial.println("exemplo.txt existe.");
+  }
+  else {
+    Serial.println("exemplo.txt não existe.");  
+  }
+
+  // Deletando o objeto file
+  Serial.println("Removendo exemplo.txt...");
+  SD.remove("exemplo.txt");
+
+  if (SD.exists("exemplo.txt")){ 
+    Serial.println("exemplo.txt existe.");
+  }
+  else {
+    Serial.println("exemplo.txt não existe.");  
+  }
+}
+
+void loop()
+{
+   // Nada acontece após a conclusão da instalação.
+}
