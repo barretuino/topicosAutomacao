f1ab2a4d2b6d3f1a66c3fb6fe2651aefd810a205
Barretuino Tecnologia <41486280+barretuino@users.noreply.github.com>
2022-05-18 20:26:45 -0300

    Add files via upload

---
+++ Aula 1/EthernetShieldMensagem/EthernetShieldMensagem.ino
@@ -0,0 +1,32 @@
+ #include <SPI.h>
+#include <Ethernet.h>
+
+byte mac[] = { 
+  0x90, 0xA2, 0xDA, 0x00, 0x9B, 0x36 }; //physical mac address
+byte ip[] = { 
+  192, 168, 2, 99 }; // ip in lan
+byte gateway[] = { 
+  192, 168, 2, 1}; // internet access via router
+byte subnet[] = { 
+  255, 255, 255, 0 }; //subnet mask
+EthernetServer server(80); //server port
+
+void setup()
+{
+  Ethernet.begin(mac, ip);
+  server.begin();
+  Serial.begin(9600);
+}
+
+void loop()
+{
+  // listen for incoming clients
+  EthernetClient client = server.available();
+   if (client) {
+    client.println("<html>");
+    client.println("<head>"); 
+    client.println("Bem Vindo!");
+    client.println("</body>");
+    client.println("</html>");
+  }
+}
---
+++ Aula 3/SensorLDRJava/SensorLDRJava.ino
@@ -0,0 +1,26 @@
+/**************************************************************
+   Projeto Arduino sensor de luminosidade --> integração Serial
+   Prof. Ms. Paulo Barreto
+   Aula: 11/10/2014
+   Exemplo de atualização de leitura de sensor LDR
+**************************************************************/
+int sensor = 0;      //Pino analógico em que o sensor está conectado.
+int valorSensor = 0; //variável usada para ler o valor do sensor em tempo real.
+
+//Função setup, executado uma vez ao ligar o Arduino.
+void setup(){
+  //Ativando o serial monitor que exibirá os valores lidos no sensor.
+  Serial.begin(9600);
+}
+
+//Função loop, executado enquanto o Arduino estiver ligado.
+void loop(){ 
+  //Lendo o valor do sensor.
+  int valorSensor = analogRead(sensor);
+  //Imprimindo valor na serial
+  Serial.print("$");
+  Serial.print(valorSensor);
+  Serial.println("#");
+  //Aguarda 'n' milissegundos
+  delay(5000);
+}
